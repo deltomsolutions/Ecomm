18ecd0e7689d1413b4d9499909e4349c6f74ba0d
IYIOLA OMOWUMI ABRAHAM <deltomsolutions@gmail.com>
2021-11-23 13:25:55 +0100

    share code on github

---
+++ ecommerce/src/data.js
@@ -0,0 +1,77 @@
+const data={
+    products:[
+        {
+          _id:'1',
+          name: 'Hp 240 G8 LAPTOP',
+          category:'Laptop' ,
+          image:'/images/p1.jpg',
+          price: 204898,
+          brand:'hP',
+          rating: 4.5,
+          numReviews:10,
+          description:'high quality product', 
+
+        },
+         {
+          _id:'2',
+          name: 'Hp Laptop',
+          category:'Laptop' ,
+          image:'/images/p2.jpg',
+          price: 500000,
+          brand:'Hp',
+          rating: 4.5,
+          numReviews:10,
+          description:'high quality product', 
+
+        },
+         {
+          _id:'3',
+          name: 'Lenovo IdeaPad 14 Notebook',
+          category:'Laptop' ,
+          image:'/images/p3.jpg',
+          price: 145000,
+          brand:'Hp',
+          rating: 4.5,
+          numReviews:10,
+          description:'high quality product', 
+
+        },
+         {
+          _id:'4',
+          name: 'Lenovo INTEL Core I3®',
+          category:'Laptop' ,
+          image:'/images/p4.jpg',
+          price: 5009000,
+          brand:'Lenovo',
+          rating: 4.5,
+          numReviews:10,
+          description:'high quality product', 
+
+        },
+         {
+          _id:'5',
+          name: 'Hp ProBook 11 X360',
+          category:'Laptop' ,
+          image:'/images/p5.jpg',
+          price: 165000,
+          brand:'Hp',
+          rating: 4.5,
+          numReviews:10,
+          description:'high quality product', 
+
+        },
+         {
+          _id:'6',
+          name: 'Lenovo Ideapad 14inch',
+          category:'Laptop' ,
+          image:'/images/p6.jpg',
+          price: 505500,
+          brand:'Lenovo',
+          rating: 4.5,
+          numReviews:10,
+          description:'high quality product', 
+
+        },
+    ],
+};
+export default data;
